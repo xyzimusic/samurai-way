486834d51d24af1d09dac2696e277271b322a361
XYZI <113294539+xyzimusic@users.noreply.github.com>
2023-06-18 20:18:07 +0300

    samurai-way-1_9

---
+++ src/Technologies.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export function Technologies() {
+    return (
+        <ul>
+            <li>css</li>
+            <li>html</li>
+            <li>js</li>
+            <li>react</li>
+        </ul>
+    );
+}
